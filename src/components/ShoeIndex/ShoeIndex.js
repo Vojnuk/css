98388a8ee11442b9e8ee72eae6d2e35083b63501
Vojnuk <martel.gilgames@gmail.com>
2023-06-04 23:00:23 +0200

    done module without optional

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,58 +1,72 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import Spacer from "../Spacer";
+import ShoeSidebar from "../ShoeSidebar";
+import ShoeGrid from "../ShoeGrid";
 
 const ShoeIndex = ({ sortId, setSortId }) => {
-  return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={34} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <MainColumn>
+                <Header>
+                    <Title>Running</Title>
+                    <Select
+                        label="Sort"
+                        value={sortId}
+                        onChange={(ev) => setSortId(ev.target.value)}
+                    >
+                        <option value="newest">Newest Releases</option>
+                        <option value="price">Price</option>
+                    </Select>
+                </Header>
+                <Spacer size={34} />
+                <ShoeGrid />
+            </MainColumn>
+            <LeftColumn>
+                <Breadcrumbs>
+                    <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+                    <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+                    <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+                </Breadcrumbs>
+                <Spacer size={42} />
+                <ShoeSidebar />
+            </LeftColumn>
+        </Wrapper>
+    );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: flex-end;
+    align-items: baseline;
+    gap: 32px;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+    width: 248px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+    flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+    display: flex;
+    justify-content: center;
+    align-items: baseline;
+    min-height: 42px;
+`;
 
 const Title = styled.h2`
-  font-size: 1.5rem;
-  font-weight: ${WEIGHTS.medium};
+    font-size: 1.5rem;
+    font-weight: ${WEIGHTS.medium};
+    margin-right: auto;
 `;
 
 export default ShoeIndex;
